dcc0d13e13d2850f54ac30f5493258b5707b13a0
Jeremy Cole <jeremycole059@gmail.com>
2025-06-12 17:24:40 -0600

    next config changes for azure deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  server: {
+    port: process.env.PORT || 8080
+  },
+  reactStrictMode: true,
+  
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  }
+};
 
 export default nextConfig;
