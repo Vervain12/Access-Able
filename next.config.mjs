dfd1428e1592774a5f9b9785e3a72d4a46f3944e
Jeremy Cole <jeremycole059@gmail.com>
2025-06-12 18:04:54 -0600

    Revert "next config changes for azure deployment"
    This reverts commit dcc0d13e13d2850f54ac30f5493258b5707b13a0.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  server: {
-    port: process.env.PORT || 8080
-  },
-  reactStrictMode: true,
-  
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
