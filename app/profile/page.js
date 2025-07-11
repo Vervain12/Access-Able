dad2a4f68a7a456d03f4b8414c6f3a1e1ec8ab0f
James Smith <157991703+Jimbo28S@users.noreply.github.com>
2025-07-10 22:16:53 -0600

    Styled Profile Page
    Mostly done. Styling based on screensize needs to be done still

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,7 +1,12 @@
 "use client";
 
 import { signOut } from "../services/account-services-client";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Switch } from "@mui/material";
+import { AiOutlineQuestionCircle } from 'react-icons/ai';
+import { BsFillChatDotsFill } from 'react-icons/bs';
+import { HiOutlineDocumentText } from 'react-icons/hi';
+import { MdReportProblem } from 'react-icons/md';
+
 
 export default function ProfilePage() {
   const reloadAfterSignout = async () => {
@@ -9,18 +14,18 @@ export default function ProfilePage() {
   };
 
   return (
-    <div className="bg-gray-200">
-      <h1 className="font-bold text-2xl pt-10 pl-10">Profile Settings</h1>
+    <div className="bg-gray-200 lg:pl-5 xl:pl-10 2xl:pl-20">
+      <h1 className="font-bold text-2xl pt-10 pl-30">Profile Settings</h1>
 
-      <div className="flex flex-row pl-30 pt-10 gap-25">
+      <div className="flex flex-row 2xl:pl-20 xl:pl-10 pt-10 gap-25">
         <div className="bg-white rounded-md w-90 h-170 p-7 flex flex-col">
           <h2 className="pb-10">Profile Information</h2>
           <div className="bg-white flex flex-col gap-10 items-center mb-5">
             <p>Profile image circle here</p>
-            <button className="w-30 h-10 bg-blue-500">Change Photo</button>
+            <button className="w-30 h-10 rounded-md text-white bg-blue-500">Change Photo</button>
           </div>
           <div>
-            <p className="pb-2">Full Name</p>
+            <p className="pb-2">Username</p>
             <TextField
               id="full-name"
               variant="outlined"
@@ -58,28 +63,110 @@ export default function ProfilePage() {
           </button>
         </div>
 
-        <div className="h-400 flex flex-col gap-10">
-          <div className="bg-red-200 w-200 h-120">
-            <h2>Account Settings</h2>
+        <div className="h-300 flex flex-col gap-10">
+          <div className="bg-white w-200 h-120">
+            <h2 className="font-bold text-2xl pt-7 pl-8 pb-5">
+              Account Settings
+            </h2>
 
-            <div className="flex flex-row p-5 border-b-2 border-gray-500">
+            <div className="flex flex-row p-5 border-b-2 border-gray-300 justify-between ml-5 mr-5">
               <div>
-                <h3>Accessibility Preferences</h3>
-                <p>Manage your accessibility needs and preferences</p>
+                <h1 className="text-lg">Accessibility Preferences</h1>
+                <p className="text-sm text-gray-700">
+                  Manage your accessibility needs and preferences
+                </p>
               </div>
 
-              <button className="w-40 h-10 border-1 border-gray-200 ml-50">
-                Edit Preferences
-              </button>
+              <div className="flex items-center">
+                <button className="w-40 h-10 border-1 border-gray-200 ml-75">
+                  Edit Preferences
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-row p-5 border-b-2 border-gray-300 justify-between ml-5 mr-5">
+              <div>
+                <h1 className="text-lg">Dark Mode</h1>
+                <p className="text-sm text-gray-700">
+                  Toggle between light and dark themes
+                </p>
+              </div>
+
+              <div className="flex items-center">
+                <Switch />
+              </div>
+            </div>
+
+            <div className="flex flex-row p-5 border-b-2 border-gray-300 justify-between ml-5 mr-5">
+              <div>
+                <h1 className="text-lg">Language</h1>
+                <p className="text-sm text-gray-700">
+                  Choose your preferred language
+                </p>
+              </div>
+
+              <div className="flex items-center">
+                <button className="w-25 h-10 border-1 border-gray-200 ml-75">
+                  English
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-row p-5 justify-between ml-5 mr-5">
+              <div>
+                <h1 className="text-lg">Email Notifications</h1>
+                <p className="text-sm text-gray-700">
+                  Receive updates about new reviews and places
+                </p>
+              </div>
+
+              <div className="flex items-center">
+                <Switch />
+              </div>
             </div>
           </div>
 
-          <div className="bg-red-200 h-50">
-            <h2>Activity Overview</h2>
+          {/* Activity Overview Section*/}
+          <div className="bg-white h-55">
+            <h2 className="font-bold text-2xl p-5 pb-8">Activity Overview</h2>
+
+            <div className="flex flex-row gap-20 justify-center">
+              <div className="w-50 h-25 bg-blue-200 flex flex-col justify-end items-center pb-5">
+                <p>(# of reviews written)</p>
+                <p>Reviews Written</p>
+              </div>
+
+              <div className="w-50 h-25 bg-green-200 flex flex-col justify-end items-center pb-5">
+                <p>(# of places visited)</p>
+                <p>Places Visited</p>
+              </div>
+
+              <div className="w-50 h-25 bg-purple-200 flex flex-col justify-end items-center pb-5">
+                <p>(# of saved places)</p>
+                <p>Saved Places</p>
+              </div>
+            </div>
           </div>
 
-          <div className="bg-red-200 h-90">
-            <h2>Support and Help</h2>
+          {/* Support and Help Section */}
+          <div className="bg-white h-80 flex flex-col gap-5">
+            <h2 className="text-2xl pl-10 pt-5 font-bold">Support & Help</h2>
+            <button className="w-180 h-10 border-1 border-gray-300 mx-10 flex items-center justify-start">
+              <AiOutlineQuestionCircle style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px"}} />
+              Help Center
+            </button>
+            <button className="w-180 h-10 border-1 border-gray-300 mx-10 flex items-center justify-start">
+              <BsFillChatDotsFill style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px"}} />
+              Contact Support
+            </button>
+            <button className="w-180 h-10 border-1 border-gray-300 mx-10 flex items-center justify-start">
+               <HiOutlineDocumentText style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px"}} />
+              Terms & Privacy
+            </button>
+            <button className="w-180 h-10 border-1 border-gray-300 mx-10 flex items-center justify-start">
+              <MdReportProblem style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px"}} />
+              Report an Issue
+            </button>
           </div>
         </div>
       </div>
